03355115ee84051f88829552ca7d55ac551e8733
zenixark <231135227+zenixarkus@users.noreply.github.com>
2025-11-13 18:34:36 -0500

    Write my own nvidia overclocking tool in c
    Technically my first ever thing written in a low level language even
    though it was simpler than I expected. OH how REFRESHING it is to learn
    something without handholding from scratch. I hope to learn more if I
    can think of more good uses for C, and later I wanna learn C++ and Rust
    down the line.

---
+++ misc/overclock.c
@@ -0,0 +1,28 @@
+#include "/opt/cuda/targets/x86_64-linux/include/nvml.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Requires cuda of all things to be installed during compilation for the nvml headers
+// clang -lnvidia-ml -o overclock overclock.c && strip overclock
+
+int check(nvmlReturn_t status) {
+    if (status != NVML_SUCCESS) {
+        printf("Error: %s\n", nvmlErrorString(status));
+        exit(1);
+    }
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    nvmlDevice_t device;
+
+    check(nvmlInit());
+    check(nvmlDeviceGetHandleByIndex(0, &device));
+    check(nvmlDeviceSetGpcClkVfOffset(device, 210));
+    check(nvmlDeviceSetMemClkVfOffset(device, 3000));
+
+    printf("Overclocks applied successfully!\n");
+    return 0;
+}
