c006f0abc9df6fd950aa29e6e78786d9bedfc6cf
zenixark <231135227+zenixarkus@users.noreply.github.com>
2025-11-15 23:23:11 -0500

    Add some more user.js optimizations

--- rootfs/home/user/.librewolf/user/user.js
+++ rootfs/home/user/.librewolf/user/user.js
@@ -28,43 +28,64 @@ user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-f
 
 // ========== PERFORMANCE ==========
 // https://github.com/CachyOS/CachyOS-Browser-Settings/blob/master/cachyos.cfg#L549
-// I think I could push this section much MUCH further
+// https://raw.githubusercontent.com/yokoffing/Betterfox/refs/heads/main/Fastfox.js
 
 user_pref("nglayout.initialpaint.delay", 0);
 user_pref("nglayout.initialpaint.delay_in_oopif", 0);
-user_pref("content.notify.interval", 100000);
-user_pref("browser.startup.preXulSkeletonUI", false);
 
-user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("gfx.content.skia-font-cache-size", 32);
+
+user_pref("content.notify.interval", 100000);
+user_pref("content.max.tokenizing.time", 1000000);
+user_pref("content.interrupt.parsing", true);
+
+user_pref("content.switch.threshold", 300000);
 
 user_pref("gfx.webrender.all", true);
-user_pref("gfx.webrender.compositor", true);
 user_pref("gfx.webrender.precache-shaders", true);
-user_pref("gfx.canvas.accelerated.cache-size", 512);
-user_pref("gfx.content.skia-font-cache-size", 20);
-user_pref("layers.gpu-process.enabled", true);
-user_pref("layers.gpu-process.force-enabled", true);
 
+user_pref("gfx.canvas.accelerated.cache-items", 32768);
+user_pref("gfx.canvas.accelerated.cache-size", 4096);
+
+user_pref("browser.cache.memory.capacity", 131072);
 user_pref("browser.cache.memory.max_entry_size", 153600);
-user_pref("image.cache.size", 10485760);
-user_pref("image.mem.decode_bytes_at_a_time", 32768);
-user_pref("image.mem.shared.unmap.min_expiration_ms", 120000);
+
+user_pref("dom.storage.default_quota", 20480);
+user_pref("dom.storage.shadow_writes", true);
+
+user_pref("media.memory_cache_max_size", 262144);
 user_pref("media.memory_caches_combined_limit_kb", 2560000);
+
 user_pref("media.cache_readahead_limit", 7200);
 user_pref("media.cache_resume_threshold", 3600);
+
+user_pref("image.cache.size", 10485760);
+user_pref("image.mem.decode_bytes_at_a_time", 65536);
+
+user_pref("network.buffer.cache.size", 65535);
+user_pref("network.buffer.cache.count", 48);
 
 user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
 user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.max-persistent-connections-per-proxy", 48);
+user_pref("network.http.request.max-start-delay", 5);
+
 user_pref("network.http.pacing.requests.enabled", false);
+
+user_pref("network.dnsCacheEntries", 10000);
 user_pref("network.dnsCacheExpiration", 3600);
+
 user_pref("network.ssl_tokens_cache_capacity", 10240);
+
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
 
 // ========== HARDENING ==========
 
-// 1600x900 is arkenfox's default new window size so I enforce letterboxing to that
-user_pref("privacy.resistFingerprinting.letterboxing", true);
-user_pref("privacy.resistFingerprinting.letterboxing.dimensions", "1600x900");
+// Stop automatic connections
+user_pref("services.settings.server", ""); // firefox.settings.services.mozilla.com
+user_pref("dom.push.connection.enabled", false); // push.services.mozilla.com
+user_pref("extensions.update.enabled", false); // uBlock doesn't update enough to justify automatic connections
 
 // I leave v6 off solely because I use a VPN, otherwise I would prefer it
 user_pref("network.dns.disableIPv6", true);
@@ -72,10 +93,9 @@ user_pref('network.dns.preferIPv6', true);
 
 user_pref("network.http.referer.XOriginPolicy", 2);
 
-// Stop automatic connections
-user_pref("services.settings.server", ""); // firefox.settings.services.mozilla.com
-user_pref("dom.push.connection.enabled", false); // push.services.mozilla.com
-user_pref("extensions.update.enabled", false); // uBlock doesn't update enough to justify automatic connections
+// 1600x900 is arkenfox's default new window size so I enforce letterboxing to that
+user_pref("privacy.resistFingerprinting.letterboxing", true);
+user_pref("privacy.resistFingerprinting.letterboxing.dimensions", "1600x900");
 
 user_pref("browser.translations.enable", false);
 user_pref("extensions.ml.enabled", false);
